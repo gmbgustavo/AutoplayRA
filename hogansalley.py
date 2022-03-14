d54a5421fb0acdedbbebbc60390b6e7e3125ad22
gmbgustavo <gmb.gustavo@gmail.com>
2022-03-14 17:45:01 -0300

    Usa variavel ao invés de ler tudo do disco toda vez.

--- hogansalley.py
+++ hogansalley.py
@@ -1,21 +1,17 @@
 import pyautogui
+from PIL import Image, ImageOps
 
 
-POSICAO = (10, 60, 280, 170)
+POSICAO = (10, 60, 550, 350)
+
+GA = ImageOps.grayscale(Image.open('./opt/ha/ga.png'))
+GB = ImageOps.grayscale(Image.open('./opt/ha/gb.png'))
+GC = ImageOps.grayscale(Image.open('./opt/ha/gc.png'))
 
 
 def main():
     while True:
-        bandido = pyautogui.locateOnScreen('./opt/ha/b1.png', region=POSICAO, confidence=0.8)
-        if bandido is not None:
-            tiro = pyautogui.center(bandido)
-            pyautogui.moveTo(tiro)
-            pyautogui.mouseDown()
-            pyautogui.mouseUp()
-            pyautogui.moveTo(800, 800)
-            print(f'GANG-B: {tiro}')
-
-        bandido = pyautogui.locateOnScreen('./opt/ha/b2.png', region=POSICAO, confidence=0.8)
+        bandido = pyautogui.locateOnScreen(GA, region=POSICAO, confidence=0.8, grayscale=True)
         if bandido is not None:
             tiro = pyautogui.center(bandido)
             pyautogui.moveTo(tiro)
@@ -24,7 +20,16 @@ def main():
             pyautogui.moveTo(800, 800)
             print(f'GANG-A: {tiro}')
 
-        bandido = pyautogui.locateOnScreen('./opt/ha/b3.png', region=POSICAO, confidence=0.8)
+        bandido = pyautogui.locateOnScreen(GB, region=POSICAO, confidence=0.8, grayscale=True)
+        if bandido is not None:
+            tiro = pyautogui.center(bandido)
+            pyautogui.moveTo(tiro)
+            pyautogui.mouseDown()
+            pyautogui.mouseUp()
+            pyautogui.moveTo(800, 800)
+            print(f'GANG-B: {tiro}')
+
+        bandido = pyautogui.locateOnScreen(GC, region=POSICAO, confidence=0.8, grayscale=True)
         if bandido is not None:
             tiro = pyautogui.center(bandido)
             pyautogui.moveTo(tiro)
